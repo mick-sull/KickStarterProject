0ded61e1d0cd8de5ffc9990809d66ca7c0dfe104
Unknown <colm.osullivan4@mycit.ie>
2016-11-23 22:26:03 +0000

    Controllers added

---
+++ KickStarterProject/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO users VALUES (1,'Colm O Sullivan','female'),(2,'Ono, Yoko','female'),(3,'Opie, Julian','male'),(4,'Etty, William','male'),(5,'Wallis, Henry','male');
+
+INSERT INTO projects VALUES (1,'Geometry of Fear'),(2,'Kinetic Art'),(3,'New British Sculpture'),(4,'Romanticism');
+
