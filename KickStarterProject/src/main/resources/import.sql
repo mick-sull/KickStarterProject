6a29ff8a7f9b600dc1a3add134baf0c51e209999
Unknown <michael.osullivan8@mycit.ie>
2016-11-23 23:24:02 +0000

    Logging In successful-ish

---
+++ KickStarterProject/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO user VALUES (NULL,'test','test');
+
+INSERT INTO projects VALUES (1,'Project 1', 'Description 1', 1200.00),(1,'Project 2', 'Description 2', 1249.00),(1,'Project 3', 'Description 3', 3000.00),(1,'Project 4', 'Description 4', 6000.00);
