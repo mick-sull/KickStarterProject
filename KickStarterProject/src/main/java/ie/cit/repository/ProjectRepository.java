c5369879e42017cee375d224ee8d615af798b48a
Unknown <colm.osullivan4@mycit.ie>
2016-11-23 21:09:55 +0000

    Some more crud stuff

--- KickStarterProject/src/main/java/ie/cit/repository/ProjectRepository.java
+++ KickStarterProject/src/main/java/ie/cit/repository/ProjectRepository.java
@@ -1,10 +1,28 @@
 package ie.cit.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import ie.cit.entity.Project;
 
-public interface ProjectRepository extends JpaRepository<Project, Long>{
-
-	// Change 17.55
+public interface ProjectRepository extends CrudRepository{  //JpaRepository<Project, Long>{
+	
+	// create method (save Project Object)
+	
+	
+	// read method (Find all Projects)
+	
+	
+	// update method  (Save a Project Object)
+	
+	
+	// delete method  ()
+	
+	
+	//@Query("SELECT p FROM Artist a JOIN a.movements m WHERE m.name = :name")
+	//public List<Project> findByMovementsName(@Param("name") String name);
 }
--- KickStarterProject/src/main/java/ie/cit/repository/UserRepository.java
+++ KickStarterProject/src/main/java/ie/cit/repository/UserRepository.java
@@ -1,9 +1,18 @@
 package ie.cit.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import ie.cit.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository{  //JpaRepository<User, Long> {
     User findByUsername(String username);
+    
+    // COLM EDITED  -  Find user(owner) by giving a project name//
+    //@Query("SELECT u FROM User u JOIN u.project p WHERE p.name = :name")
+	//public List<User> findByProjectName(@Param("name") String name);
 }
