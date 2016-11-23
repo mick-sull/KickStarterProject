6cd997667738886d3ba673594c581aee71fd9aca
Unknown <colm.osullivan4@mycit.ie>
2016-11-23 17:07:39 +0000

    project entity

---
+++ KickStarterProject/src/main/java/ie/cit/repository/ProjectRepository.java
@@ -0,0 +1,9 @@
+package ie.cit.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ie.cit.entity.Project;
+
+public interface ProjectRepository extends JpaRepository<Project, Long>{
+
+}
