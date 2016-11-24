c8e3993443f86d4ea6e2b2f52b350129168934f2
Unknown <colm.osullivan4@mycit.ie>
2016-11-24 12:51:41 +0000

    home page up

--- KickStarterProject/src/main/java/ie/cit/controller/HomeController.java
+++ KickStarterProject/src/main/java/ie/cit/controller/HomeController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
-	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	@RequestMapping(value = "/"/*, method = RequestMethod.GET*/)
 	public String home(Locale locale, Model model) {
 		return "home";
 	}
