820de777cdf0be17d13629adbafcb823f980af42
mrzimmy <kevopm@eircom.net>
2016-12-02 16:25:25 +0000

    Dates and images added

--- KickStarterProject/src/main/resources/import.sql
+++ KickStarterProject/src/main/resources/import.sql
@@ -1,6 +1,11 @@
 INSERT INTO users VALUES (NULL,'password', 'smithy12'),(NULL,'pass', 'user'),(NULL,'password', 'murph12'),(NULL,'password', 'jonesy12'),(NULL,'password', 'damo12');
 
-INSERT INTO projects VALUES (NULL,'Description 1', 1200.00, 'Project 1', 1),(NULL,'Description 2', 1249.00, 'Project 2', 2),(NULL,'Description 3', 3000.00, 'Project 3', 3),(NULL,'Description 4', 6000.00, 'Project 4', 4);
+INSERT INTO projects VALUES (NULL, timestamp'2016-09-09 09:30:25 GMT', '2016-12-15','Description 1', 1200.00, '../images/money_tree.jpg', 'Money Tree', 1);
+INSERT INTO projects VALUES (NULL, timestamp'2016-10-10 09:30:25 GMT', '2016-12-25','Description 2', 1249.00, '../images/cats_eyes.png', 'Cat Contact Lenses', 2);
+INSERT INTO projects VALUES (NULL, timestamp'2016-11-11 09:30:25 GMT', '2016-12-31','Description 3', 3000.00, '../images/tea_bags.jpg', 'Washable Teabags', 3);
+INSERT INTO projects VALUES (NULL, timestamp'2016-12-1 09:30:25 GMT', '2017-02-1','Description 4', 6000.00, '../images/toliet_tissue.jpg', 'Re-usable Toilet Paper', 4);
 
-INSERT INTO pledges VALUES (NULL, 15.0, 2, 4), (NULL, 120.0, 1, 2), (NULL, 75.0, 4, 2);
-INSERT INTO pledges VALUES (NULL, 25.0, 2, 3), (NULL, 20.0, 1, 3), (NULL, 50.0, 4, 3);
+
+
+INSERT INTO pledges VALUES (NULL, 15.0, timestamp'2016-10-29 09:30:25 GMT', 2, 4), (NULL, 120.0, timestamp'2016-11-09 09:30:25 GMT', 1, 2), (NULL, 75.0, timestamp'2016-10-30 09:30:25 GMT', 4, 2);
+INSERT INTO pledges VALUES (NULL, 25.0, timestamp'2016-12-01 09:30:25 GMT', 2, 3), (NULL, 20.0, timestamp'2016-12-01 09:30:25 GMT', 1, 3), (NULL, 50.0, timestamp'2016-12-01 09:30:25 GMT', 4, 3);
